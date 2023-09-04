79cbabf0e0aea71475127f8b8e5e47004ea6b2bb
铭哲 倪 <142307993+FrostLunar@users.noreply.github.com>
2023-09-04 08:21:00 +0000

    Update 3 files

--- argo.sh
+++ argo.sh
@@ -48,39 +48,39 @@ EOF
 }
 
 export_list() {
-  VMESS="{ \"v\": \"2\", \"ps\": \"Argo-Vmess\", \"add\": \"icook.hk\", \"port\": \"443\", \"id\": \"c4130adb-241b-4361-a540-5a897da166b8\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/glitch-vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
+  VMESS="{ \"v\": \"2\", \"ps\": \"Argo-Vmess\", \"add\": \"icook.hk\", \"port\": \"443\", \"id\": \"de04add9-5c68-8bab-950c-08cd5320df18\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${ARGO_DOMAIN}\", \"path\": \"/codesandbox-vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${ARGO_DOMAIN}\", \"alpn\": \"\" }"
   cat > list << EOF
 *******************************************
 V2-rayN:
 ----------------------------
-vless://c4130adb-241b-4361-a540-5a897da166b8@icook.hk:443?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fglitch-vless?ed=2048#Argo-Vless
+vless://de04add9-5c68-8bab-950c-08cd5320df18@172.67.65.34:443?encryption=none&security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fcodesandbox-vless?ed=2048#Argo-Vless
 ----------------------------
 vmess://$(echo $VMESS | base64 -w0)
 ----------------------------
-trojan://c4130adb-241b-4361-a540-5a897da166b8@icook.hk:443?security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fglitch-trojan?ed=2048#Argo-Trojan
+trojan://de04add9-5c68-8bab-950c-08cd5320df18@icook.hk:443?security=tls&sni=${ARGO_DOMAIN}&type=ws&host=${ARGO_DOMAIN}&path=%2Fcodesandbox-trojan?ed=2048#Argo-Trojan
 ----------------------------
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpjNDEzMGFkYi0yNDFiLTQzNjEtYTU0MC01YTg5N2RhMTY2YjhAaWNvb2suaGs6NDQzCg==@icook.hk:443#Argo-Shadowsocks
-由于该软件导出的链接不全，请自行处理如下: 传输协议: WS ， 伪装域名: ${ARGO_DOMAIN} ，路径: /glitch-shadowsocks?ed=2048 ， 传输层安全: tls ， sni: ${ARGO_DOMAIN}
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpkZTA0YWRkOS01YzY4LThiYWItOTUwYy0wOGNkNTMyMGRmMThAaWNvb2suaGs6NDQzCg==@icook.hk:443#Argo-Shadowsocks
+由于该软件导出的链接不全，请自行处理如下: 传输协议: WS ， 伪装域名: ${ARGO_DOMAIN} ，路径: /codesandbox-shadowsocks?ed=2048 ， 传输层安全: tls ， sni: ${ARGO_DOMAIN}
 *******************************************
 小火箭:
 ----------------------------
-vless://c4130adb-241b-4361-a540-5a897da166b8@icook.hk:443?encryption=none&security=tls&type=ws&host=${ARGO_DOMAIN}&path=/glitch-vless?ed=2048&sni=${ARGO_DOMAIN}#Argo-Vless
+vless://de04add9-5c68-8bab-950c-08cd5320df18@icook.hk:443?encryption=none&security=tls&type=ws&host=${ARGO_DOMAIN}&path=/codesandbox-vless?ed=2048&sni=${ARGO_DOMAIN}#Argo-Vless
 ----------------------------
-vmess://bm9uZTpjNDEzMGFkYi0yNDFiLTQzNjEtYTU0MC01YTg5N2RhMTY2YjhAaWNvb2suaGs6NDQzCg==?remarks=Argo-Vmess&obfsParam=${ARGO_DOMAIN}&path=/glitch-vmess?ed=2048&obfs=websocket&tls=1&peer=${ARGO_DOMAIN}&alterId=0
+vmess://bm9uZTpkZTA0YWRkOS01YzY4LThiYWItOTUwYy0wOGNkNTMyMGRmMThAaWNvb2suaGs6NDQzCg==?remarks=Argo-Vmess&obfsParam=${ARGO_DOMAIN}&path=/codesandbox-vmess?ed=2048&obfs=websocket&tls=1&peer=${ARGO_DOMAIN}&alterId=0
 ----------------------------
-trojan://c4130adb-241b-4361-a540-5a897da166b8@icook.hk:443?peer=${ARGO_DOMAIN}&plugin=obfs-local;obfs=websocket;obfs-host=${ARGO_DOMAIN};obfs-uri=/glitch-trojan?ed=2048#Argo-Trojan
+trojan://de04add9-5c68-8bab-950c-08cd5320df18@icook.hk:443?peer=${ARGO_DOMAIN}&plugin=obfs-local;obfs=websocket;obfs-host=${ARGO_DOMAIN};obfs-uri=/codesandbox-trojan?ed=2048#Argo-Trojan
 ----------------------------
-ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpjNDEzMGFkYi0yNDFiLTQzNjEtYTU0MC01YTg5N2RhMTY2YjhAaWNvb2suaGs6NDQzCg==?obfs=wss&obfsParam=${ARGO_DOMAIN}&path=/glitch-shadowsocks?ed=2048#Argo-Shadowsocks
+ss://Y2hhY2hhMjAtaWV0Zi1wb2x5MTMwNTpkZTA0YWRkOS01YzY4LThiYWItOTUwYy0wOGNkNTMyMGRmMThAaWNvb2suaGs6NDQzCg==?obfs=wss&obfsParam=${ARGO_DOMAIN}&path=/codesandbox-shadowsocks?ed=2048#Argo-Shadowsocks
 *******************************************
 Clash:
 ----------------------------
-- {name: Argo-Vless, type: vless, server: icook.hk, port: 443, uuid: c4130adb-241b-4361-a540-5a897da166b8, tls: true, servername: ${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: {path: /glitch-vless?ed=2048, headers: { Host: ${ARGO_DOMAIN}}}, udp: true}
+- {name: Argo-Vless, type: vless, server: icook.hk, port: 443, uuid: de04add9-5c68-8bab-950c-08cd5320df18, tls: true, servername: ${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: {path: /codesandbox-vless?ed=2048, headers: { Host: ${ARGO_DOMAIN}}}, udp: true}
 ----------------------------
-- {name: Argo-Vmess, type: vmess, server: icook.hk, port: 443, uuid: c4130adb-241b-4361-a540-5a897da166b8, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: /glitch-vmess?ed=2048, headers: {Host: ${ARGO_DOMAIN}}}, udp: true}
+- {name: Argo-Vmess, type: vmess, server: icook.hk, port: 443, uuid: de04add9-5c68-8bab-950c-08cd5320df18, alterId: 0, cipher: none, tls: true, skip-cert-verify: true, network: ws, ws-opts: {path: /codesandbox-vmess?ed=2048, headers: {Host: ${ARGO_DOMAIN}}}, udp: true}
 ----------------------------
-- {name: Argo-Trojan, type: trojan, server: icook.hk, port: 443, password: c4130adb-241b-4361-a540-5a897da166b8, udp: true, tls: true, sni: ${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: { path: /glitch-trojan?ed=2048, headers: { Host: ${ARGO_DOMAIN} } } }
+- {name: Argo-Trojan, type: trojan, server: icook.hk, port: 443, password: de04add9-5c68-8bab-950c-08cd5320df18, udp: true, tls: true, sni: ${ARGO_DOMAIN}, skip-cert-verify: false, network: ws, ws-opts: { path: /codesandbox-trojan?ed=2048, headers: { Host: ${ARGO_DOMAIN} } } }
 ----------------------------
-- {name: Argo-Shadowsocks, type: ss, server: icook.hk, port: 443, cipher: chacha20-ietf-poly1305, password: c4130adb-241b-4361-a540-5a897da166b8, plugin: v2ray-plugin, plugin-opts: { mode: websocket, host: ${ARGO_DOMAIN}, path: /glitch-shadowsocks?ed=2048, tls: true, skip-cert-verify: false, mux: false } }
+- {name: Argo-Shadowsocks, type: ss, server: icook.hk, port: 443, cipher: chacha20-ietf-poly1305, password: de04add9-5c68-8bab-950c-08cd5320df18, plugin: v2ray-plugin, plugin-opts: { mode: websocket, host: ${ARGO_DOMAIN}, path: /codesandbox-shadowsocks?ed=2048, tls: true, skip-cert-verify: false, mux: false } }
 *******************************************
 EOF
   cat list
